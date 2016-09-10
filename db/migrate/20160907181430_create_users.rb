600a797a8aac409bd397f8d4c7fcf5ef474e645a
Rodrigo-Vargas <rodrigovargas123@gmail.com>
2016-09-10 19:12:19 -0300

    Fixed users migrations

--- db/migrate/20160907181430_create_users.rb
+++ db/migrate/20160907181430_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-      t.string :email
+      t.string :email, index:true, unique: true
       t.string :password
       t.string :auth_token
       t.timestamps
