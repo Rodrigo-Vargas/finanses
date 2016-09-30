b178b3322a47ff329f9b913f70f3399a82a1ed5f
Rodrigo-Vargas <rodrigovargas123@gmail.com>
2016-09-29 22:20:32 -0300

    Added responsive layout and improve then

--- app/assets/javascripts/app.angular.js
+++ app/assets/javascripts/app.angular.js
@@ -7,9 +7,15 @@ angular
   .controller('HeaderCtrl', ['$scope', '$window', 'UserInfoService', function($scope, $window, UserInfoService){
     $scope.currentUserInfo = UserInfoService.get();
 
+    $scope.toogleMenu = function(){
+      if (angular.element( document.querySelector( '.side-nav' ) ).hasClass('opened'))
+        angular.element( document.querySelector( '.side-nav' ) ).removeClass('opened');  
+      else
+        angular.element( document.querySelector( '.side-nav' ) ).addClass('opened');      
+    }
+
     $scope.logout = function(){
       UserInfoService.clear();
-      $window.location.href = '/login';
-  
+      $window.location.href = '/login';  
     }
   }]);
